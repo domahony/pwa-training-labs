64d489c521bb6a14d3f5746a6553475f4502d28d
Dave O'Mahony <david.c.omahony@gmail.com>
2020-03-07 17:45:37 -0330

    webapps 04.0 workbox

--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -1,1 +1,80 @@
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  /(.*)articles(.*)\.(?:png|gif|jpg)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  /images\/icon\/(.*)/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'icon-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 5
+      }),
+    ]
+  })
+);
+
+const articleHandler = workbox.strategies.networkFirst({
+  cacheName: 'articles-cache',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 50,
+    })
+  ]
+});
+
+workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+  return articleHandler.handle(args)
+  .then(response => {
+    if (!response) {
+      return caches.match('pages/offline.html');
+    } else if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  });
+});
+
+const postsHandler = workbox.strategies.cacheFirst({
+  cacheName: 'posts-cache',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 50,
+    })
+  ]
+});
+
+workbox.routing.registerRoute(/pages\/post(.*)\.html/, args => {
+  return postsHandler.handle(args)
+  .then(
+    response => {
+    if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  },
+    reject => {
+      return caches.match('pages/offline.html');
+  });
+});
